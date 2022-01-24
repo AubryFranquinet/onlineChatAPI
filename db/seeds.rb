48a05a2028a0e0b9b6e56d20bef7f30e00d24b4a
Aubry Franquinet <aubry.franquinet@hotmail.com>
2022-01-24 13:32:57 +0100

    added data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create([
+    {
+        firstName: "Aubry",
+        lastName: "Franquinet",
+        email: "aubry.franquinet@hotmail.com",
+        birthday: "26/09/2000",
+        bio: "Junior web developer at BeCode and Admin of the app"
+    },
+    {
+        firstName: "Kévin",
+        lastName: "Monsieur",
+        email: "kevin.monsieur@gmail.com",
+        birthday: "",
+        bio: "Junior web developer at BeCode and Admin of the app"
+    }
+])
