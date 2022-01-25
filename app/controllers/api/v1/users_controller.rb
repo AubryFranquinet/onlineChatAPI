f66d5e784412e35c4e80e8d7d3841c36a5e75db5
Aubry Franquinet <aubry.franquinet@hotmail.com>
2022-01-25 14:16:40 +0100

    Added DELETE route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         users = User.new(
             firstName: params[:firstName],
-            lastname: params[:lastname],
+            lastName: params[:lastName],
             email: params[:email],
             birthday: params[:birthday],
             bio: params[:bio],
@@ -28,6 +28,34 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+   def update
+    users = User.find_by(id: params[:id])
+    if users
+      users.update(
+        firstName: params[:firstName],
+        lastName: params[:lastName],
+        email: params[:email],
+        birthday: params[:birthday],
+        bio: params[:bio],
+      )
+      render json: users, status: 200
+    else
+      render json: { errors: "user not found." }, status: 404
+    end
+  end
+
+  def destroy
+    users = User.find_by(id: params[:id])
+    if users
+      users.destroy
+      render json: { message: "user deleted" }, status: 200
+    else
+      render json: { errors: "user not found." }, status: 404
+    end
+  end
+end
+
+
   private
     def prod_params
         params.require(:users).permit([
@@ -39,4 +67,4 @@ class Api::V1::UsersController < ApplicationController
             ])
     end
 
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :users
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
+      resources :users, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
    post 'authenticate', to: 'authentication#authenticate'
-
+    
 end
