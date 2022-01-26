0147425ff7c6d00ee4c2252a5d17d0d4488d2bf0
Aubry Franquinet <aubry.franquinet@hotmail.com>
2022-01-26 11:23:24 +0100

    Removed token auth for POST request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::API
  before_action :authenticate_request
   attr_reader :current_admin
 
-  private
+skip_before_action :authenticate_request, only: [:create]
 
+
+
+  private
   def authenticate_request
     @current_admin = AuthorizeApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_admin
+    
+    
   end
 end
